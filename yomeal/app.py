441ecefe25dd876744a041530bfe3b9effe84191
brpat07 <20097156+brpat07@users.noreply.github.com>
2020-02-01 18:55:28 -0800

    ym-6: Added simple sms handler + dir setup + updated .gitignore

--- yomeal/app.py
+++ yomeal/app.py
@@ -1,10 +1,20 @@
-from flask import Flask
+from flask import Flask, request
+from sms_handler import handler
 app = Flask(__name__)
+
+
+@app.route("/sms", methods=['GET', 'POST'])
+def sms_handler_route():
+    if request.method == 'POST':
+        return handler.read_sms(request.form)
+    else:
+        return "Wrong method"
 
 
 @app.route('/')
 def hello():
     return "Begining of YoMeal!"
 
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
---
+++ yomeal/sms_handler/handler.py
@@ -0,0 +1,6 @@
+
+
+def read_sms(msg):
+    # Following will be msg body
+    print(msg['Body'])
+    return "test!!!"
