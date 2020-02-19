566b68feafc8adf409e53736382ffd55f1c445b4
brpat07 <20097156+brpat07@users.noreply.github.com>
2020-02-18 23:08:45 -0800

    ym-6: Added example of parseMsg + nlp

--- yomeal/parser/parser.py
+++ yomeal/parser/parser.py
@@ -1,15 +1,37 @@
 # import re
 
 
-def is_word(msg):
-    mySet = set(line.strip() for line in open(
+def parseMsg(msg):
+    foodSet = set(line.strip() for line in open(
         '/Users/bittu/Downloads/gito/YoMeal/yomeal/parser/food.txt'
         ))
-
+    # TODO: use regex
     for word in msg.split():
-        if word in mySet:
+        if word in foodSet:
             print(word)
 
 
 test = "this is banana"
-is_word(test)
+parseMsg(test)
+
+'''
+# # Simple example using spaCy
+import spacy
+nlp = spacy.load("en_core_web_sm") 
+    # Loading English tokenizer, tagger, parser, NER and word vectors
+# # ## OR ##
+# import en_core_web_sm
+# nlp = en_core_web_sm.load()
+
+# Process whole documents
+text = ("this is made from banana, 1gm eggs.")
+doc = nlp(text)
+
+# Analyze syntax
+print("Noun phrases:", [chunk.text for chunk in doc.noun_chunks])
+# print("Verbs:", [token.lemma_ for token in doc if token.pos_ == "VERB"])
+
+# Find named entities, phrases and concepts
+# for entity in doc.ents:
+#     print(entity.text, entity.label_)
+'''
